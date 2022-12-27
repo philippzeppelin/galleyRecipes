58cc0b1afb85ae1a2812df5ac3403e76dc983735
KhazovDV <101877272+KhazovDV@users.noreply.github.com>
2022-12-27 20:53:23 +0300

    Initial Commit

---
+++ galleyRecipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  galleyRecipes
+//
+//  Created by garpun on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
