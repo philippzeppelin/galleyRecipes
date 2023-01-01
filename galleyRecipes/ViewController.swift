59e7059b348316b69d2d5d67a1b091d0fa611b31
philippzeppelin <philippzeppelin@gmail.com>
2023-01-01 23:03:13 +0300

    Added TableView

--- galleyRecipes/ViewController.swift
+++ galleyRecipes/ViewController.swift
@@ -10,60 +10,74 @@ import UIKit
 class ViewController: UIViewController {
     
    //image
-    let imageView: UIImageView = {
+    let recipeImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "image.jpg") // put image name here
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
         return imageView
     }()
-
-    //tableView
-    let tableVIew: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        return tableView
-    }
     
+    //tableView
+    let recipeTableView: UITableView = {
+        let tableView = UITableView.init(frame: .zero,style: UITableView.Style.plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    let recipeIngredients = ["Butter", "Bread"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
         imageViewConstraints()
+        tableViewConstraints()
+        recipeTableView.dataSource = self
+        recipeTableView.register(UITableViewCell.self, forCellReuseIdentifier: "id")
     }
     
     // MARK: - Constraints
     //image constraints
     func imageViewConstraints() {
-        view.addSubview(imageView)
+        view.addSubview(recipeImageView)
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
-            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            imageView.heightAnchor.constraint(equalToConstant: 200)
+            recipeImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            recipeImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            recipeImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            recipeImageView.heightAnchor.constraint(equalToConstant: 200)
         ])
-        
-        
     }
     
-    
+    func tableViewConstraints() {
+        view.addSubview(recipeTableView)
+        NSLayoutConstraint.activate([
+            recipeTableView.topAnchor.constraint(equalTo: recipeImageView.bottomAnchor, constant: -20),
+            recipeTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            recipeTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            recipeTableView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
     
 
     
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return recipeIngredients.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: idScheduleCell, for: indexPath) as! ScheduleTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "id", for: indexPath)
+        // конфигурируем ячейку
+        cell.textLabel?.text  = self.recipeIngredients[indexPath.row]        // возвращаем сконфигурированный экземпляр ячейки
         return cell
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
-    }
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 90
+//    }
 }
