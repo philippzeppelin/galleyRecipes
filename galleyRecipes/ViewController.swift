8d14ddc29ae039a0276a35d8483597431c01042c
philippzeppelin <philippzeppelin@gmail.com>
2023-01-01 21:42:16 +0300

    Added UIImageView
    and a little bit UITableVIew

--- galleyRecipes/ViewController.swift
+++ galleyRecipes/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   //image
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "image.jpg") // put image name here
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
 
+    //tableView
+    let tableVIew: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        imageViewConstraints()
     }
+    
+    // MARK: - Constraints
+    //image constraints
+    func imageViewConstraints() {
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        
+    }
+    
+    
+    
 
-
+    
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: idScheduleCell, for: indexPath) as! ScheduleTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+}
